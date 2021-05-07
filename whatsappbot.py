98ce3d5dc8b9cba503ab266b39b2c6f09a82bd5c
Waliff Cordeiro <waliffcordeiro@gmail.com>
2021-05-07 10:20:01 -0300

    Remove print

--- whatsappbot.py
+++ whatsappbot.py
@@ -7,7 +7,6 @@ driver.get('https://web.whatsapp.com/')
 sleep(TEMPO_INICIAL)
 
 receptores = open('receptores.txt', 'r').read().split('\n')
-print (receptores)
 
 mensagem = open('mensagem.txt', 'r').read()
 
