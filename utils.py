d58da39b469a2a2ad3f4ef6d32041c410061c090
Waliff Cordeiro <waliffcordeiro@gmail.com>
2021-05-07 10:16:47 -0300

    Adicionando tempo entre os envios

--- utils.py
+++ utils.py
@@ -6,18 +6,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+TEMPO_ESPERA = 2.5
+
 def buscar_contato(contato):
     campo_pesquisa = driver.find_element_by_xpath('//div[contains(@class, "copyable-text selectable-text")]')
-    sleep(3)
+    sleep(TEMPO_ESPERA)
     campo_pesquisa.click()
     campo_pesquisa.send_keys(contato)
+    sleep(TEMPO_ESPERA)
     campo_pesquisa.send_keys(Keys.ENTER)
 
 def enviar_mensagem(mensagem):
     campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class, "copyable-text selectable-text")]')
     campo_mensagem[1].click()
-    sleep(3)
+    sleep(TEMPO_ESPERA)
     for linha in mensagem.split('\n'):
         ActionChains(driver).send_keys(linha).perform()
         ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.SHIFT).key_up(Keys.ENTER).perform()
+    sleep(TEMPO_ESPERA)
     ActionChains(driver).send_keys(Keys.RETURN).perform()
